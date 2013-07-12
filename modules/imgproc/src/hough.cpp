f17279db3bca59589e88004ab939e7a461ab3382
Martin Muñoz <im.mmun@gmail.com>
2013-07-12 18:04:07 -0400

    Add ranged variant of the standard hough transform

--- modules/imgproc/src/hough.cpp
+++ modules/imgproc/src/hough.cpp
@@ -164,6 +164,96 @@ icvHoughLinesStandard( const CvMat* img, float rho, float theta,
     }
 }
 
+/****************************************************************************************\
+*                       Ranged variant of Classical Hough Transform                     *
+\****************************************************************************************/
+
+
+static void
+icvHoughLinesStandardRanged( const CvMat* img,
+                             float rho, float theta, int threshold,
+                             float angStart, float angEnd,
+                             CvSeq *lines, int linesMax )
+{
+    cv::AutoBuffer<int> _accum, _sort_buf;
+    cv::AutoBuffer<float> _tabSin, _tabCos;
+ 
+    const uchar* image;
+    int step, width, height;
+    int numangle, numrho;
+    int total = 0;
+    int i, j;
+    float irho = 1 / rho;
+    double scale;
+ 
+    CV_Assert( CV_IS_MAT(img) && CV_MAT_TYPE(img->type) == CV_8UC1 );
+ 
+    image = img->data.ptr;
+    step = img->step;
+    width = img->cols;
+    height = img->rows;
+ 
+    numangle = cvRound((angEnd - angStart) / theta);
+    numrho = cvRound(((width + height) * 2 + 1) / rho);
+ 
+    _accum.allocate((numangle+2) * (numrho+2));
+    _sort_buf.allocate(numangle * numrho);
+    _tabSin.allocate(numangle);
+    _tabCos.allocate(numangle);
+    int *accum = _accum, *sort_buf = _sort_buf;
+    float *tabSin = _tabSin, *tabCos = _tabCos;
+ 
+    memset( accum, 0, sizeof(accum[0]) * (numangle+2) * (numrho+2) );
+ 
+    float ang = angStart;
+    for(int n = 0; n < numangle; ang += theta, n++ )
+    {
+        tabSin[n] = (float)(sin((double)ang) * irho);
+        tabCos[n] = (float)(cos((double)ang) * irho);
+    }
+ 
+    // stage 1. fill accumulator
+    for( i = 0; i < height; i++ )
+        for( j = 0; j < width; j++ )
+        {
+            if( image[i * step + j] != 0 )
+                for(int n = 0; n < numangle; n++ )
+                {
+                    int r = cvRound( j * tabCos[n] + i * tabSin[n] );
+                    r += (numrho - 1) / 2;
+                    accum[(n+1) * (numrho+2) + r+1]++;
+                }
+        }
+ 
+    // stage 2. find local maximums
+    for(int r = 0; r < numrho; r++ )
+        for(int n = 0; n < numangle; n++ )
+        {
+            int base = (n+1) * (numrho+2) + r+1;
+            if( accum[base] > threshold &&
+                accum[base] > accum[base - 1] && accum[base] >= accum[base + 1] &&
+                accum[base] > accum[base - numrho - 2] && accum[base] >= accum[base + numrho + 2] )
+                sort_buf[total++] = base;
+        }
+ 
+    // stage 3. sort the detected lines by accumulator value
+    icvHoughSortDescent32s( sort_buf, total, accum );
+ 
+    // stage 4. store the first min(total,linesMax) lines to the output buffer
+    linesMax = MIN(linesMax, total);
+    scale = 1./(numrho+2);
+    for( i = 0; i < linesMax; i++ )
+    {
+        CvLinePolar line;
+        int idx = sort_buf[i];
+        int n = cvFloor(idx*scale) - 1;
+        int r = idx - (n+1)*(numrho+2) - 1;
+        line.rho = (r - (numrho - 1)*0.5f) * rho;
+        line.angle = n * theta + angStart;
+        cvSeqPush( lines, &line );
+    }
+}
+
 
 /****************************************************************************************\
 *                     Multi-Scale variant of Classical Hough Transform                   *
@@ -758,8 +848,12 @@ cvHoughLines2( CvArr* src_image, void* lineStorage, int method,
     switch( method )
     {
     case CV_HOUGH_STANDARD:
-          icvHoughLinesStandard( img, (float)rho,
-                (float)theta, threshold, lines, linesMax );
+          icvHoughLinesStandard( img, (float)rho, (float)theta,
+                threshold, lines, linesMax );
+          break;
+    case CV_HOUGH_STANDARD_RANGED:
+          icvHoughLinesStandardRanged( img, (float)rho, (float)theta,
+                threshold, iparam1, iparam2, lines, linesMax );
           break;
     case CV_HOUGH_MULTI_SCALE:
           icvHoughLinesSDiv( img, (float)rho, (float)theta,
